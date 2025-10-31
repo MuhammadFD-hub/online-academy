a0d8bdee9dcf4e0d3cd6fc2c7bbf6794f3f85208
M. Fardeen Dar <fardeendar30@proton.me>
2025-10-31 06:27:51 -0700

    using hardcoded CORS for backend

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,8 @@ const coursesRoutes = require("./routes/coursesRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const lessonRoutes = require("./routes/lessonRoutes");
-let DB_URL = "",
-  CLIENT_URL = "";
 
-if (!process.env.CLIENT_URL) CLIENT_URL = "http://localhost:5173";
-else CLIENT_URL = process.env.CLIENT_URL;
+let DB_URL = "";
 
 if (!process.env.MONGO_URI) DB_URL = "mongodb://127.0.0.1:27017/online-academy";
 else DB_URL = process.env.MONGO_URI;
@@ -22,7 +19,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+      "http://localhost:5173",
+      "https://0nline-academy.netlify.app",
+      "https://online-academy-theta.vercel.app",
+    ],
     credentials: true,
   })
 );
