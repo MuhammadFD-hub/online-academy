c78c1ab7b8041e553ffd15673dbe64667838f89b
M. Fardeen Dar <fardeendar30@proton.me>
2025-11-01 02:11:04 -0700

    fixed CORS

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://0nline-academy.netlify.app/"],
+    origin: ["http://localhost:5173", "https://0nline-academy.netlify.app"],
     credentials: true,
   })
 );
