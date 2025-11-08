b0f0780740bb92c03122e348b21996d339dcd9fc
M. Fardeen Dar <fardeendar30@proton.me>
2025-11-07 18:37:11 -0700

    using keyframes animation instead of motion div in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { Navbar, Container, Image, Dropdown, Nav } from "react-bootstrap";
-import { motion } from "framer-motion";
 import { FiMoreVertical } from "react-icons/fi";
 import styles from "./Header.module.css";
 import { useEffect, useRef, useState } from "react";
@@ -33,118 +32,108 @@ export default function Header() {
     };
   }, [showMenu]);
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, type: "spring" }}
-    >
-      <Navbar className={`shadow-sm px-3 py-2 ${styles.header}`}>
-        <Container>
-          <Navbar.Brand
-            as={Link}
-            to="/"
-            className={`fw-bold text-primary fs-4 d-flex align-items-center ${styles.Brand}`}
-          >
-            Online Academy
-          </Navbar.Brand>
+    <Navbar className={`shadow-sm px-3 py-2 ${styles.header}`}>
+      <Container>
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          className={`fw-bold text-primary fs-4 d-flex align-items-center ${styles.Brand}`}
+        >
+          Online Academy
+        </Navbar.Brand>
 
-          <Dropdown align="end" className="ms-auto" show={true}>
-            <div className="d-flex gap-1 align-items-center justify-content-center">
-              {user && (
-                <Nav.Link
-                  className={`${styles.pfpLink}`}
+        <Dropdown align="end" className="ms-auto" show={true}>
+          <div className="d-flex gap-1 align-items-center justify-content-center">
+            {user && (
+              <Nav.Link className={`${styles.pfpLink}`} as={Link} to="/profile">
+                <Image
+                  src={
+                    getCloudUrl(
+                      pfpCloudData?.public_id,
+                      pfpCloudData?.format
+                    ) || "/default-pfp.svg"
+                  }
+                  roundedCircle
+                  width={32}
+                  height={32}
+                  alt="Profile"
+                />
+              </Nav.Link>
+            )}
+            <button
+              className={`btn btn-link border-0 ${styles.menuButton}`}
+              onClick={() => setShowMenu(!showMenu)}
+              ref={menuBtnRef}
+            >
+              <FiMoreVertical size={24} />
+            </button>
+          </div>
+          <Dropdown.Menu
+            className={`${showMenu && styles.showMenu} ${
+              styles.dropdownMenu
+            } border-0 py-2`}
+            ref={dropdownRef}
+          >
+            {user ? (
+              <>
+                <h3 className={`${styles.dropdownTitle}`}>
+                  <Username
+                    classname={`px-3 py-1 ${styles.username}`}
+                    displayChars={15}
+                  />
+                </h3>
+                <Dropdown.Divider />
+                <Dropdown.Item
                   as={Link}
                   to="/profile"
+                  className={`${styles.dropdownItem}`}
+                  onClick={() => setShowMenu(false)}
                 >
-                  <Image
-                    src={
-                      getCloudUrl(
-                        pfpCloudData?.public_id,
-                        pfpCloudData?.format
-                      ) || "/default-pfp.svg"
-                    }
-                    roundedCircle
-                    width={32}
-                    height={32}
-                    alt="Profile"
-                  />
-                </Nav.Link>
-              )}
-              <button
-                className={`btn btn-link border-0 ${styles.menuButton}`}
-                onClick={() => setShowMenu(!showMenu)}
-                ref={menuBtnRef}
-              >
-                <FiMoreVertical size={24} />
-              </button>
-            </div>
-            <Dropdown.Menu
-              className={`${showMenu && styles.showMenu} ${
-                styles.dropdownMenu
-              } border-0 py-2`}
-              ref={dropdownRef}
-            >
-              {user ? (
-                <>
-                  <h3 className={`${styles.dropdownTitle}`}>
-                    <Username
-                      classname={`px-3 py-1 ${styles.username}`}
-                      displayChars={15}
-                    />
-                  </h3>
-                  <Dropdown.Divider />
-                  <Dropdown.Item
-                    as={Link}
-                    to="/profile"
-                    className={`${styles.dropdownItem}`}
-                    onClick={() => setShowMenu(false)}
-                  >
-                    Profile
-                  </Dropdown.Item>
-                  {/* <Dropdown.Item
+                  Profile
+                </Dropdown.Item>
+                {/* <Dropdown.Item
                           as={Link}
                           to="/settings"
                           className={`${styles.dropdownItem}`}
                         >
                           Settings
                         </Dropdown.Item> */}
-                  <Dropdown.Divider />
-                  <Dropdown.Item
-                    as={Link}
-                    to="/login"
-                    onClick={() => {
-                      setShowMenu(false);
-                      logout();
-                    }}
-                    className={`${styles.dropdownItem}`}
-                  >
-                    Logout
-                  </Dropdown.Item>
-                </>
-              ) : (
-                <>
-                  <Dropdown.Item
-                    as={Link}
-                    to="/login"
-                    className={`${styles.dropdownItem}`}
-                    onClick={() => setShowMenu(false)}
-                  >
-                    Login
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    as={Link}
-                    to="/signup"
-                    className={`${styles.dropdownItem}`}
-                    onClick={() => setShowMenu(false)}
-                  >
-                    Signup
-                  </Dropdown.Item>
-                </>
-              )}
-            </Dropdown.Menu>
-          </Dropdown>
-        </Container>
-      </Navbar>
-    </motion.div>
+                <Dropdown.Divider />
+                <Dropdown.Item
+                  as={Link}
+                  to="/login"
+                  onClick={() => {
+                    setShowMenu(false);
+                    logout();
+                  }}
+                  className={`${styles.dropdownItem}`}
+                >
+                  Logout
+                </Dropdown.Item>
+              </>
+            ) : (
+              <>
+                <Dropdown.Item
+                  as={Link}
+                  to="/login"
+                  className={`${styles.dropdownItem}`}
+                  onClick={() => setShowMenu(false)}
+                >
+                  Login
+                </Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/signup"
+                  className={`${styles.dropdownItem}`}
+                  onClick={() => setShowMenu(false)}
+                >
+                  Signup
+                </Dropdown.Item>
+              </>
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Navbar>
   );
 }
